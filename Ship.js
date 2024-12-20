0589a61c2949c16e8a4bfe183a760049876f4a34
fnovoas <fnovoas@unal.edu.co>
2024-12-20 16:52:04 -0500

    Update Ship.js
    intentar arreglar los powerups

--- Ship.js
+++ Ship.js
@@ -1,32 +1,35 @@
 class Ship {
-    constructor(pos, s_height, s_width, lifes, imagesAlive, imagesDead) {
-      this.pos = pos.copy();
-      this.vel = createVector(0, 0).limit(this.maxVelocity);
-      this.acc = createVector(0.0, 0).limit(this.maxAcceleration);
-      this.lifes = lifes;
-      this.s_height = s_height;
-      this.s_width = s_width;
-      this.imagesAlive = imagesAlive;
-      this.imagesDead = imagesDead;
-      this.maxImageStates = imagesAlive.length;
-      this.movement = [false, false, false, false]; // 0->left, 1->up, 2->down, 3->right
-      this.isShooting = false;
-      this.shieldActivation = 0;
-      this.shieldDuration = 5000;
-      this.maxVelocity = 9;
-      this.maxAcceleration = 1;
-      this.potAcc = 0.5;
-      this.lastShotTime = 0;
-      this.shootDelay = 1000;
-      this.minShootDelay = 500;
-      this.imageDelay = 1000;
-      this.prevMillis = 0;
-      this.imageState = 0;
-      this.isDead = false;
-      this.hasShield = false;
-      this.deadAnimStartTime = 0;
-      this.deadAnimDuration = 500;
-    }
+  constructor(pos, s_height, s_width, lifes, imagesAlive, imagesDead) {
+    this.pos = pos.copy();
+    this.vel = createVector(0, 0).limit(this.maxVelocity);
+    this.acc = createVector(0.0, 0).limit(this.maxAcceleration);
+    this.lifes = lifes;
+    this.s_height = s_height;
+    this.s_width = s_width;
+    this.imagesAlive = imagesAlive;
+    this.imagesDead = imagesDead;
+    this.maxImageStates = imagesAlive.length;
+    this.movement = [false, false, false, false]; // 0->left, 1->up, 2->down, 3->right
+    this.isShooting = false;
+    this.shieldActivation = 0;
+    this.shieldDuration = 5000;
+    this.maxVelocity = 9;
+    this.maxAcceleration = 1;
+    this.potAcc = 0.5;
+    this.lastShotTime = 0;
+    this.shootDelay = 1000;
+    this.minShootDelay = 500;
+    this.imageDelay = 1000;
+    this.prevMillis = 0;
+    this.imageState = 0;
+    this.isDead = false;
+    this.hasShield = false;
+    this.deadAnimStartTime = 0;
+    this.deadAnimDuration = 500;
+    this.speedBoostActive = false;
+    this.speedBoostDuration = 5000;
+    this.speedBoostStartTime = 0;
+  }
   
     updateAcc() {
       let x = this.potAcc * (int(this.movement[3]) - int(this.movement[0]));
@@ -120,32 +123,33 @@ class Ship {
           this.shieldActivation = millis();
           this.hasShield = true;
           break;
+  
         case PowerUpType.CADENCY:
           if (this.shootDelay > this.minShootDelay) {
             this.shootDelay -= 100;
           }
           break;
+  
         case PowerUpType.DOUBLE_POINTS:
           doublePointsActivation = millis();
           doublePoints = true;
           break;
+  
         case PowerUpType.FREEZE:
           alienGroup.freezeActivation = millis();
-          alienGroup.freezealienshipgorup();
+          alienGroup.freezealienshipgroup(); // Asegurar que este método funcione correctamente
           break;
+  
         case PowerUpType.SPEED:
-          for (let i = 0; i < alienShipGrid.length; i++) {
-            for (let j = 0; j < alienShipGrid[i].length; j++) {
-              let alien = alienShipGrid[i][j];
-              if (alien != null) {
-                alien.vel.x *= 0.9;
-              }
-            }
-          }
+          this.speedBoostActive = true;
+          this.speedBoostStartTime = millis();
+          this.maxVelocity += 3;
           break;
+  
         case PowerUpType.NOENEMYBULLETS:
           enemyBullets = [];
           break;
+  
         case PowerUpType.EXTRA_LIFE:
           this.lifes++;
           break;
@@ -187,6 +191,10 @@ class Ship {
       if (millis() - this.shieldActivation > this.shieldDuration) {
         this.hasShield = false;
       }
+      if (this.speedBoostActive && millis() - this.speedBoostStartTime > this.speedBoostDuration) {
+        this.speedBoostActive = false;
+        this.maxVelocity -= 3;
+      }
       if (millis() - this.prevMillis > this.imageDelay) {
         this.imageState = (this.imageState + 1) % this.maxImageStates;
         this.prevMillis = millis();
@@ -205,7 +213,13 @@ class Ship {
       } else {
         push();
         if (this.hasShield) {
-          tint(0, 0, 255);
+          tint(0, 0, 255, 150); // Efecto visual para el escudo
+        }
+        if (this.speedBoostActive) {
+          stroke(255, 0, 0);
+          strokeWeight(2);
+          noFill();
+          rect(this.pos.x - 5, this.pos.y - 5, this.s_width + 10, this.s_height + 10); // Efecto visual para velocidad
         }
         image(this.imagesAlive[this.imageState], this.pos.x, this.pos.y, this.s_width, this.s_height);
         pop();
